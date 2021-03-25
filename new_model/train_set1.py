9535df00443004b1eff81bdef2b6278e40796c76
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-03-25 09:46:17 +0530

    Removed incremental training code

--- new_model/train_set1.py
+++ new_model/train_set1.py
@@ -29,18 +29,13 @@ def variable(t: torch.Tensor, use_cuda=True, **kwargs):
 parser = argparse.ArgumentParser(description='Video action recogniton training')
 parser.add_argument('--logfile_name', type=str, default="generator_w_with_sem:10",
                     help='file name for storing the log file')
-parser.add_argument('--load_name', type = str, default = None, help = 'file name for loading the log file')
 parser.add_argument('--gpu', type=int, default=3,
                     help='GPU ID, start from 0')
-parser.add_argument('--epochs', type = int, default = 30, help='number of epochs to train the model for')
-parser.add_argument('--increment_epochs', type = int, default = 10, help = 'number of epochs to increase after each set')
+parser.add_argument('--epochs', type = int, default = 100, help='number of epochs to train the model for')
 parser.add_argument('--snapshot', type = int, default = 50, help = 'model is saved after these many epochs')
-parser.add_argument('--resume_epoch', type = int, default = None, help = 'resume training from this epoch')
-parser.add_argument('--num_classes', type = int, default = 10, help = 'number of classes in the pretrained classifier')
-parser.add_argument('--total_classes', type = int, default = 50, help = 'total number of classes to train over')
+parser.add_argument('--total_classes', type = int, default = 40, help = 'total number of classes to train over')
 parser.add_argument('--test_interval', type = int, default = 1, help = 'number of epochs after which to test the model')
 parser.add_argument('--train', type = int, default = 1, help = '1 if training. 0 for testing')
-parser.add_argument('--importance', type = int, default = 100, help = 'multiplying factor for distillation loss')
 args = parser.parse_args()
 
 gpu_id = str(args.gpu)
@@ -55,9 +50,7 @@ b1=0.5
 b2=0.999
 batch_size = 100
 input_dim = 2048
-increment = args.increment_epochs
-nEpochs = args.epochs - increment  # Number of epochs for training
-resume_epoch = args.resume_epoch  # Default is 0, change if want to resume
+nEpochs = args.epochs  # Number of epochs for training
 useTest = True # See evolution of the test set when training
 nTestInterval = args.test_interval # Run on test set every nTestInterval epochs
 snapshot = args.snapshot # Store a model every snapshot epochs
@@ -65,7 +58,6 @@ lr = 1e-3 # Learning rate
 
 dataset = 'ucf101' # Options: hmdb51 or ucf101
 
-num_classes = args.num_classes
 total_classes = args.total_classes
 
 all_classes = np.arange(total_classes)
@@ -91,7 +83,6 @@ print ("all_classes:", all_classes)
 current_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 save_dir_root = current_dir
 save_dir = os.path.join(save_dir_root, 'run', log_name)
-load_dir = os.path.join(save_dir_root, 'run', args.load_name)
 modelName = 'Bi-LSTM' # Options: C3D or R2Plus1D or R3D
 saveName = modelName + '-' + dataset
 
@@ -114,8 +105,8 @@ def kaiming_normal_init(m):
 
 
 
-def train_model(dataset=dataset, save_dir=save_dir, load_dir = load_dir, num_classes=num_classes, lr=lr,
-                num_epochs=nEpochs, increment = increment, save_epoch=snapshot, useTest=useTest, test_interval=nTestInterval):
+def train_model(dataset=dataset, save_dir=save_dir, num_classes=total_classes, lr=lr,
+                num_epochs=nEpochs, save_epoch=snapshot, useTest=useTest, test_interval=nTestInterval):
     """
         Args:
             num_classes (int): Number of classes in the data
@@ -123,155 +114,126 @@ def train_model(dataset=dataset, save_dir=save_dir, load_dir = load_dir, num_cla
     """
 
     model = ConvLSTM(
-        num_classes=40,
+        num_classes=num_classes,
         latent_dim=512,
         lstm_layers=1,
         hidden_dim=1024,
         bidirectional=True,
         attention=True,
     )
-    classifier = Modified_Classifier(num_classes = 40)
+    classifier = Classifier(num_classes = num_classes)
 
-    if args.resume_epoch is not None and args.train == 1:
-        checkpoint = torch.load(os.path.join(load_dir, saveName + '_epoch-' + str(args.resume_epoch - 1) + '.pth.tar'),
-                       map_location=lambda storage, loc: storage)   # Load all tensors onto the CPU
-        print("Initializing weights from: {}...".format(
-            os.path.join(save_dir, 'models', saveName + '_epoch-' + str(resume_epoch - 1) + '.pth.tar')))
-        model.load_state_dict(checkpoint['extractor_state_dict'])
-        classifier.load_state_dict(checkpoint['classifier_state_dict'])
-        
-    elif args.resume_epoch is not None and args.train == 0:
-        checkpoint = torch.load(os.path.join(load_dir, saveName + '_increment' + '_epoch-' + str(args.resume_epoch - 1) + '.pth.tar'),
-                       map_location=lambda storage, loc: storage)   # Load all tensors onto the CPU
-        print("Initializing weights from: {}...".format(
-            os.path.join(save_dir, 'models', saveName + '_epoch-' + str(resume_epoch - 1) + '.pth.tar')))
-        
-        model.load_state_dict(checkpoint['extractor_state_dict'])
-        classifier.load_state_dict(checkpoint['classifier_state_dict'])
-
-    else:
-        print("Training {} from scratch...".format(modelName))
+    print("Training {} from scratch...".format(modelName))
 
     print('Total params: %.2fM' % (sum(p.numel() for p in model.parameters()) / 1000000.0))
 
     log_dir = os.path.join(save_dir)
     writer = SummaryWriter(log_dir=log_dir)
 
-    num_classes = args.num_classes
-    total_classes = args.total_classes
+    print('Training model on {} dataset...'.format(dataset))
 
-    init_classes = 40
-    iters = total_classes//num_classes
+    train_dataset = video_dataset(train = True, classes = all_classes[:num_classes])
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_dataset = video_dataset(train = False, classes = all_classes[:num_classes])
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
-    for i in range(1):
-
-        print('Training model on {} dataset...'.format(dataset))
-
-        train_dataset = video_dataset(train = True, classes = all_classes[:init_classes])
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-        test_dataset = video_dataset(train = False, classes = all_classes[:init_classes])
-        test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
-
-        trainval_loaders = {'train': train_dataloader, 'val': test_dataloader}
-        trainval_sizes = {x: len(trainval_loaders[x].dataset) for x in ['train', 'val']}
-        test_size = len(test_dataloader.dataset)
-        lab_list = []
-        pred_list = []
+    trainval_loaders = {'train': train_dataloader, 'test': test_dataloader}
+    trainval_sizes = {x: len(trainval_loaders[x].dataset) for x in ['train', 'test']}
+    lab_list = []
+    pred_list = []
 
         # att = np.load("../npy_files/seen_semantic_51.npy")
         # att = torch.tensor(att).cuda()    
 
-        if cuda:
-            model = model.to(device)
-            classifier = classifier.to(device)
+    if cuda:
+        model = model.to(device)
+        classifier = classifier.to(device)
 
-        optimizer = torch.optim.Adam(list(model.parameters())+list(classifier.parameters()), lr=lr)
-        num_epochs = num_epochs + increment
+    optimizer = torch.optim.Adam(list(model.parameters())+list(classifier.parameters()), lr=lr)
 
-        if args.train == 1:
+    if args.train == 1:
             
-            for epoch in range(num_epochs):
+        for epoch in range(num_epochs):
+            start_time = timeit.default_timer()
+
+            running_loss = 0.0
+            running_corrects = 0.0
+
+            model.train()
+            classifier.train()
+
+            for indices, inputs, labels in (trainval_loaders["train"]):
+                inputs = inputs.permute(0,2,1,3,4)
+                image_sequences = Variable(inputs.to(device), requires_grad=True)
+                labels = Variable(labels.to(device), requires_grad=False)                
+
+                model.lstm.reset_hidden_state()
+                loop_batch_size = len(inputs)
+
+                true_features_2048 = model(image_sequences)
+                true_features_2048 = true_features_2048.view(true_features_2048.size(0), -1)
+                logits = classifier(true_features_2048)
+
+                loss = nn.CrossEntropyLoss()(logits, labels)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                _, predictions = torch.max(torch.softmax(logits, dim = 1), dim = 1, keepdim = False)
+                                  
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(predictions == labels.data)
+
+            epoch_loss = running_loss / trainval_sizes["train"]
+            epoch_acc = running_corrects.double() / trainval_sizes["train"]
+
+            writer.add_scalar('data/train_acc_epoch_benchmark {}'.format(i), epoch_acc, epoch)
+            print("[test] Epoch: {}/{} Training Acc: {}".format(epoch+1, num_epochs, epoch_acc))
+
+
+            if useTest and epoch % test_interval == (test_interval - 1):
+                model.eval()
+                classifier.eval()
                 start_time = timeit.default_timer()
 
                 running_loss = 0.0
                 running_corrects = 0.0
 
-                model.train()
-                classifier.train()
-
-                for indices, inputs, labels in (trainval_loaders["train"]):
+                for indices, inputs, labels in (trainval_loader["test"]):
                     inputs = inputs.permute(0,2,1,3,4)
-                    image_sequences = Variable(inputs.to(device), requires_grad=True)
-                    labels = Variable(labels.to(device), requires_grad=False)                
-
-                    model.lstm.reset_hidden_state()
+                    image_sequences = Variable(inputs.to(device), requires_grad=False)
+                    labels = Variable(labels.to(device), requires_grad=False)              
                     loop_batch_size = len(inputs)
 
-                    true_features_2048 = model(image_sequences)
-                    true_features_2048 = true_features_2048.view(true_features_2048.size(0), -1)
-                    logits = classifier(true_features_2048)
+                    with torch.no_grad():
+                        model.lstm.reset_hidden_state()
+                        true_features_2048 = model(image_sequences)
+                        probs = classifier(true_features_2048)
 
-                    cls_loss = nn.CrossEntropyLoss()(logits, labels)
 
-                    loss = cls_loss
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-
-                    _, predictions = torch.max(torch.softmax(logits, dim = 1), dim = 1, keepdim = False)
-                                  
-                    running_loss += loss.item() * inputs.size(0)
+                    _, predictions = torch.max(torch.softmax(probs, dim = 1), dim = 1, keepdim = False)
                     running_corrects += torch.sum(predictions == labels.data)
 
-                epoch_loss = running_loss / trainval_sizes["train"]
-                epoch_acc = running_corrects.double() / trainval_sizes["train"]
+                real_epoch_acc = running_corrects.double() / trainval_sizes["train"]
 
-                writer.add_scalar('data/train_acc_epoch_benchmark {}'.format(i), epoch_acc, epoch)
-                print("[test] Epoch: {}/{} Training Acc: {}".format(epoch+1, num_epochs, epoch_acc))
+                writer.add_scalar('data/test_acc_epoch_benchmark {}'.format(i), real_epoch_acc, epoch)
 
-
-                if useTest and epoch % test_interval == (test_interval - 1):
-                    model.eval()
-                    classifier.eval()
-                    start_time = timeit.default_timer()
-
-                    running_loss = 0.0
-                    running_corrects = 0.0
-
-                    for indices, inputs, labels in test_dataloader:
-                        inputs = inputs.permute(0,2,1,3,4)
-                        image_sequences = Variable(inputs.to(device), requires_grad=False)
-                        labels = Variable(labels.to(device), requires_grad=False)              
-                        loop_batch_size = len(inputs)
-
-                        with torch.no_grad():
-                            model.lstm.reset_hidden_state()
-                            true_features_2048 = model(image_sequences)
-                            probs = classifier(true_features_2048)
+                print("[test] Epoch: {}/{} Test Dataset Acc: {}".format(epoch+1, num_epochs, real_epoch_acc))
+                stop_time = timeit.default_timer()
+                print("Execution time: " + str(stop_time - start_time) + "\n")
 
 
-                        _, predictions = torch.max(torch.softmax(probs, dim = 1), dim = 1, keepdim = False)
-                        running_corrects += torch.sum(predictions == labels.data)
-
-                    real_epoch_acc = running_corrects.double() / test_size
-
-                    writer.add_scalar('data/test_acc_epoch_benchmark {}'.format(i), real_epoch_acc, epoch)
-
-                    print("[test] Epoch: {}/{} Test Dataset Acc: {}".format(epoch+1, num_epochs, real_epoch_acc))
-                    stop_time = timeit.default_timer()
-                    print("Execution time: " + str(stop_time - start_time) + "\n")
-
-
-                if (epoch % save_epoch == save_epoch - 1):
-                    save_path = os.path.join(save_dir, saveName + '_increment' '_epoch-' + str(epoch) + '.pth.tar')
-                    torch.save({
+            if (epoch % save_epoch == save_epoch - 1):
+                save_path = os.path.join(save_dir, saveName + '_increment' '_epoch-' + str(epoch) + '.pth.tar')
+                torch.save({
                         'epoch': epoch + 1,
                         'extractor_state_dict': model.state_dict(),
                         'classifier_state_dict': classifier.state_dict(),
                         'init_classes': init_classes,
                         'num_classes': num_classes,
                     }, save_path)
-                    print("Save model at {}\n".format(save_path))
+                print("Save model at {}\n".format(save_path))
         
     writer.close()
 
