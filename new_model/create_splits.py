e8b86d85b37d7f5e8546980621112245ce68238c
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-03-25 08:56:49 +0530

    Compatible with split_data.py

--- new_model/create_splits.py
+++ new_model/create_splits.py
@@ -5,14 +5,14 @@ import pdb
 for (dirs,a,b) in os.walk('/home/SharedData/fabio/zsl_cgan/ucf_split1/train'): 
 	a.sort()
 	count = 0
-	for folder in a[0:50]:
-		for mode in ["train", "test_seen"]:
+	for folder in a[0:70]:
+		for mode in ["train", "test"]:
 			dir_add = f"/home/SharedData/fabio/zsl_cgan/ucf_split1/{mode}/" + folder
 			# pdb.set_trace()
-			dest = f"/home/SharedData/fabio/zsl_cgan/ucf_split1/data_3_{mode.split('_')[0]}/" + folder
+			dest = f"/home/SharedData/fabio/zsl_cgan/ucf_split1/data_0_{mode.split('_')[0]}/" + folder
 			print(f"\ndir_add: {dir_add}\ndest: {dest} \n")
 			destination = shutil.copytree(dir_add, dest)
 		count += 1
-
 	break
+
 print(f"folders copied: {count}")
